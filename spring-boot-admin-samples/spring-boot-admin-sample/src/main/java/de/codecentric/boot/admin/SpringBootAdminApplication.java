18ea75e5d600e4c3a1a885d2b69e13d382eecf0a
gmlove <gracekinglau@gmail.com>
2016-04-10 11:05:01 +0800

    upgrade spring boot dependency to 1.3.3.RELEASE and spring cloud dependency to Angel.SR6

--- spring-boot-admin-samples/spring-boot-admin-sample/src/main/java/de/codecentric/boot/admin/SpringBootAdminApplication.java
+++ spring-boot-admin-samples/spring-boot-admin-sample/src/main/java/de/codecentric/boot/admin/SpringBootAdminApplication.java
@@ -19,15 +19,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
 @Configuration
 @EnableAutoConfiguration
-@EnableAdminServer
 public class SpringBootAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
 
 }
