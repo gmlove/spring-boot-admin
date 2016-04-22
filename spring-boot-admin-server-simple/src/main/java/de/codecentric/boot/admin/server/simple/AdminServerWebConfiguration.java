8bdf8058657c38358d9f012fd52a2487bf8a5ee1
gmlove <gracekinglau@gmail.com>
2016-04-22 12:18:56 +0800

    add simple server to serve admin site and provide proxy

---
+++ spring-boot-admin-server-simple/src/main/java/de/codecentric/boot/admin/server/simple/AdminServerWebConfiguration.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.codecentric.boot.admin.server.simple;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@Configuration
+@EnableConfigurationProperties
+public class AdminServerWebConfiguration extends WebMvcConfigurerAdapter
+        implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @Autowired
+    private ServerProperties server;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        if (!hasConverter(converters, MappingJackson2HttpMessageConverter.class)) {
+            ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
+                    .applicationContext(this.applicationContext).build();
+            converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+        }
+    }
+
+    private boolean hasConverter(List<HttpMessageConverter<?>> converters,
+                                 Class<? extends HttpMessageConverter<?>> clazz) {
+        for (HttpMessageConverter<?> converter : converters) {
+            if (clazz.isInstance(converter)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/spring-boot-admin-server-ui/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:index.html");
+    }
+
+}
---
+++ spring-boot-admin-server-simple/src/main/java/de/codecentric/boot/admin/server/simple/ProxyController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.codecentric.boot.admin.server.simple;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@RestController
+public class ProxyController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @CrossOrigin(origins = "*", methods = {GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE})
+    @RequestMapping("/proxy/")
+    @ResponseBody
+    public String mirrorRest(@RequestBody(required = false) String body,
+                             HttpMethod method, HttpServletRequest request,
+                             HttpServletResponse response) throws URISyntaxException
+    {
+        String url = request.getHeader("forward-url");
+        System.out.println("forward url: " + url);
+        URI uri = new URI(StringUtils.isEmpty(request.getQueryString()) ? url : url + "?" + request.getQueryString());
+
+        ResponseEntity<String> responseEntity =
+                restTemplate.exchange(uri, method, new HttpEntity<String>(body), String.class);
+
+        return responseEntity.getBody();
+    }
+}
+
