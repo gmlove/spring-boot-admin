ba3d56a0832897b83cbb791331da9d457f90ac9c
gmlove <gracekinglau@gmail.com>
2016-04-22 15:43:33 +0800

    make deployable war file

--- spring-boot-admin-server-simple/src/main/java/de/codecentric/boot/admin/server/simple/Application.java
+++ spring-boot-admin-server-simple/src/main/java/de/codecentric/boot/admin/server/simple/Application.java
@@ -24,13 +24,21 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
